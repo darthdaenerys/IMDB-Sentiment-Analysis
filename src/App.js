2a23639ed272c836c193d06a93b0e9c04950277d
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-05-12 09:16:09 +0530

    Add new textform component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
 	return (
 		<>
 			<Navbar title="AI Sentiment Analysis"/>
+			<div className='container'>
+				<TextForm title="Enter text below to analyze" buttonText="Analyze"/>
+			</div>
 		</>
 	);
 }
