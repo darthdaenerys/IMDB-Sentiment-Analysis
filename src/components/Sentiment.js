dbf57ac74fa7e87c974ed44ce939f052c21d3dfb
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-05-13 10:28:54 +0530

    Update sentiment component

--- src/components/Sentiment.js
+++ src/components/Sentiment.js
@@ -5,6 +5,10 @@ export default function Sentiment(props) {
 		<>
 			<div id="sentiment" style={props.sentimentColor}>
 				<h1>Sentiment</h1>
+				<div className='sentiment-wrapper'>
+					<p id='response'>{props.response}</p>
+					<p id='confidence'>{props.confidence}</p>
+				</div>
 			</div>
 		</>
 	)
