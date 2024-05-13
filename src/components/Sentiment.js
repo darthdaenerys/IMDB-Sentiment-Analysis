3404c71b98181b67a97331b6136c7a395fb451a1
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-05-13 10:28:42 +0530

    Add sentiment component

---
+++ src/components/Sentiment.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Sentiment(props) {
+	return (
+		<>
+			<div id="sentiment" style={props.sentimentColor}>
+				<h1>Sentiment</h1>
+			</div>
+		</>
+	)
+}
