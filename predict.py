2357aa7d62536081baaf545b9f1f43b204d9dc0d
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-05-16 11:55:19 +0530

    Add custom standardization method for input data

--- predict.py
+++ predict.py
@@ -1,7 +1,25 @@
+from tensorflow.keras.models import load_model
+import numpy as np
+import tensorflow as tf
+import string
+import re
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
+stemmer=PorterStemmer()
+
+def custom_standardize(input_data):
+    text=tf.strings.lower(input_data)
+    text=tf.strings.regex_replace(text,"<br />"," ")
+    text=tf.strings.regex_replace(text,f"[{re.escape(string.punctuation)}]","")
+    for word in stopwords.words('english'):
+        text=tf.strings.regex_replace(text,f' {word} ',"")
+    for word in stopwords.words('english'):
+        text=tf.strings.regex_replace(text,f' {word} ',f" {stemmer.stem(word)} ")
+    return text
 
 def get_sentiment(review):
-        pass
+    preds=end2end_model(np.array([review]))
 
 while True:
     line = input()
