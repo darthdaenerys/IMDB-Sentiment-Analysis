1dffb153faef121fc1cb3ab762f133e2cc49ed90
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-05-19 13:16:50 +0530

    Initial commit

---
+++ src/components/TextForm.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+	return (
+		<>
+			<div className='textform'>
+				<h2>{props.title}</h2>
+			</div>
+		</>
+	)
+}
