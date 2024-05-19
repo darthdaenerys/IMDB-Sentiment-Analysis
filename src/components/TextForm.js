63e07fc3d4aabfaaf768c54c5389e55a343a118e
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-05-19 13:17:46 +0530

    Add textarea for text form component

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props) {
+	const [text, setText] = useState("");
+	const handleOnChange = (event) => {
+		setText(event.target.value);
+	}
 	return (
 		<>
 			<div className='textform'>
 				<h2>{props.title}</h2>
+				<textarea value={text} name="query" id="textForm" cols="60" rows="14" onChange={handleOnChange}></textarea>
 			</div>
 		</>
 	)
